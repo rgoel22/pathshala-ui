abd111e339a2844bd8d57ccbbff8de7bfeb8e082
Rishabh Goel <rgoel22@umd.edu>
2023-12-03 16:07:35 -0500

    react commit

---
+++ src/components/AdminPage.js
@@ -0,0 +1,221 @@
+// AdminPage.js
+import React, { useCallback, useContext } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
+import { UserContext, useUser } from "../context/user/user.context";
+
+const AdminPage = () => {
+  const { user, logout } = useUser();
+  const { changeUser } = useContext(UserContext);
+
+  const handleLogout = useCallback(() => {
+    // logout();
+    changeUser(null);
+    console.log("Logout clicked");
+  }, []);
+
+  return (
+    <div className="admin-page">
+      <Container
+        disableGutters
+        maxWidth="sm"
+        component="main"
+        sx={{ pt: 8, pb: 6 }}
+      >
+      <Typography
+        variant="h4"
+        align="center"
+        sx={{ margin: "20px 0", marginTop: "50px" }}
+      >
+        Admin's Dashboard
+      </Typography>
+      </Container>
+      {/*<div className="user-info">
+        {user && (
+          <>
+            <Typography
+              variant="body1"
+              color="text.primary"
+              style={{ marginRight: "16px" }}
+            >
+              Welcome, {user.username}
+            </Typography>
+            <Button variant="outlined" color="primary" onClick={handleLogout}>
+              Logout
+            </Button>
+          </>
+        )}
+      </div>{" "}
+      */}
+      {/* Existing card components remain unchanged */}
+      <div className='admin-card-container'>
+      <CardContainer1
+        to="/admin/manageUsers"
+        title="Manage Users"
+        titleStyle={{ fontSize: "24px", fontWeight: "bold", color: "#4CAF50" }}
+        description="Manage users in the system"
+        descriptionStyle={{ fontSize: "16px", color: "#555" }}
+      >
+        <ImgMediaCard />
+      </CardContainer1>
+      <CardContainer2
+        to="/admin/manageInstructor" // Specify the target URL
+        title="Manage Instructors"
+        titleStyle={{ fontSize: "24px", fontWeight: "bold", color: "#2196F3" }}
+        description="Manage instructors in the system"
+        descriptionStyle={{ fontSize: "16px", color: "#555" }}
+      >
+        <ImgMediaCard />
+      </CardContainer2>
+      <CardContainer
+        to="/admin/courses"
+        title="Manage Courses"
+        titleStyle={{ fontSize: "24px", fontWeight: "bold", color: "#FF5722" }}
+        description="Manage courses in the system"
+        descriptionStyle={{ fontSize: "16px", color: "#555" }}
+      >
+        <ImgMediaCard />
+      </CardContainer>
+      </div>
+    </div>
+  );
+};
+
+const CardContainer2 = ({
+  title,
+  titleStyle,
+  description,
+  descriptionStyle,
+  cardText,
+  children,
+}) => {
+  return (
+    <Link
+      to={"/admin/manageInstructor"}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div className="card-container">
+        <Typography
+          gutterBottom
+          variant="h4"
+          component="div"
+          style={titleStyle}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={descriptionStyle}
+        >
+          {description}
+        </Typography>
+        {children}
+        <Typography variant="body2" color="text.secondary">
+          {cardText}
+        </Typography>
+      </div>
+    </Link>
+  );
+};
+
+const CardContainer = ({
+  title,
+  titleStyle,
+  description,
+  descriptionStyle,
+  cardText,
+  children,
+}) => {
+  return (
+    <Link
+      to={"/admin/courses"}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div className="card-container">
+        <Typography
+          gutterBottom
+          variant="h4"
+          component="div"
+          style={titleStyle}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={descriptionStyle}
+        >
+          {description}
+        </Typography>
+        {children}
+        <Typography variant="body2" color="text.secondary">
+          {cardText}
+        </Typography>
+      </div>
+    </Link>
+  );
+};
+
+const CardContainer1 = ({
+  title,
+  titleStyle,
+  description,
+  descriptionStyle,
+  cardText,
+  children,
+}) => {
+  return (
+    <Link
+      to={"/admin/manageUsers"}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div className="card-container">
+        <Typography
+          gutterBottom
+          variant="h4"
+          component="div"
+          style={titleStyle}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={descriptionStyle}
+        >
+          {description}
+        </Typography>
+        {children}
+        <Typography variant="body2" color="text.secondary">
+          {cardText}
+        </Typography>
+      </div>
+    </Link>
+  );
+};
+
+const ImgMediaCard = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        // Use the correct relative path from the public URL
+        image={process.env.PUBLIC_URL + "/user-manage.jpg"}
+      />
+      <CardContent>
+        {/* Text specific to each card */}
+        {/* You can customize this text based on the specific content you want for each card */}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AdminPage;
---
+++ src/components/Signin.js
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useLoading } from '../context/loadingContext';
+import Loading from './Loading';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import useAlert from '../hooks/useAlert';
+import { USER_TYPES } from "../constants";
+import { UserContext } from "../context/user/user.context";
+const defaultTheme = createTheme();
+
+export default function SignIn(props) {
+    const { loading, setLoading } = useLoading();
+    const { setAlert } = useAlert();
+    const navigate = useNavigate(); // Get the navigate function
+    const { changeUser } = React.useContext(UserContext);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const body = {
+            userId: data.get('userid'),
+            password: data.get('password'),
+        };
+
+        setLoading(true);
+
+        try {
+            const response = await fetch(
+                "https://pathshala-api-8e4271465a87.herokuapp.com/pathshala/user/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(body),
+                }
+            );
+
+            if (response.ok) {
+                setAlert('Login success!', 'success')
+                const data = await response.json();
+                localStorage.setItem('user', JSON.stringify(data));
+                if (data.userType === USER_TYPES.ADMIN) {
+                    changeUser(data);
+                    navigate("/admin");
+                } else if (data.userType === USER_TYPES.INSTRUCTOR) {
+                    changeUser(data);
+                    navigate(`/instructor/${data.userId}`);
+                } else if (data.userType === USER_TYPES.STUDENT) {
+                    changeUser(data);
+                }
+            } else {
+                // Handle unsuccessful login
+                setAlert('Invalid Credentials', 'error')
+                console.error('Login failed');
+            }
+        } catch (error) {
+            console.error('An error occurred during login:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            {loading && <Loading />}
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar alt='Pathshalo' src={require("../assets/images/pathshala.jpg")} sx={{ width: 200, height: 200 }} variant="square" />
+                    <Typography component="h1" variant="h5">
+                        Sign In
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
+                    >
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="userid"
+                            label="User ID"
+                            name="userid"
+                            autoComplete="userid"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Link href="/signup" variant="body2">
+                            {"Don't have an account? Sign Up"}
+                        </Link>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
